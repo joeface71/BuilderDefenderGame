7611e04eba7a0c7dfd523899faa7b88dfd33bd50
joeface71 <joefacenda@yahoo.com>
2021-12-31 17:19:20 -0500

    first commit

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    private BuildingTypeSO buildingType;
+    private BuildingTypeListSO buildingTypeList;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name); // TODO research why we are doing this this way
+        buildingType = buildingTypeList.list[0];
+    }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            buildingType = buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            buildingType = buildingTypeList.list[1];
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+        return mouseWorldPosition;
+    }
+}
---
+++ Assets/Scripts/BuildingTypeSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/BuildingType")]
+public class BuildingTypeSO : ScriptableObject
+{
+    public string nameString; // name field is already used by Scriptable Object Class
+    public Transform prefab;
+    public ResourceGeneratorData resourceGeneratorData;
+}
